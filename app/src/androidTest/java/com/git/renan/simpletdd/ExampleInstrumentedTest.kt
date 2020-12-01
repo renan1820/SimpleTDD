ea393eee1644cc50af7d4d1badd9360390fbebe6
Renan Castro <renancas_ribeiro@hotmail.com>
2020-11-30 22:30:00 -0300

    put libs and first test

--- app/src/androidTest/java/com/git/renan/simpletdd/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/git/renan/simpletdd/ExampleInstrumentedTest.kt
@@ -1,5 +1,12 @@
 package com.git.renan.simpletdd
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +14,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,23 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val rule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.git.renan.simpletdd", appContext.packageName)
     }
+
+    @Test
+    fun useEditText(){
+        onView(withId(R.id.edit_user_name)).perform(typeText("Renan"))
+        onView(withId(R.id.edit_user_surname)).perform(typeText("Castro"))
+        onView(withId(R.id.edit_user_age)).perform(typeText("25"))
+    }
+
 }
--- app/src/main/java/com/git/renan/simpletdd/MainActivity.kt
+++ app/src/main/java/com/git/renan/simpletdd/MainActivity.kt
@@ -2,10 +2,16 @@ package com.git.renan.simpletdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_ok).setOnClickListener(){
+            Toast.makeText(this,"Hey",Toast.LENGTH_SHORT).show()
+        }
     }
 }
